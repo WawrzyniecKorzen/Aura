7434a751fcc23dfe2fc3e5dec032777c93ea0bff
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2025-07-03 17:55:07 +0200

    AuraPlayerState, AuraAbilitySystemComponent and AuraAttributeSet classes added

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -0,0 +1,5 @@
+// Wawrzyniec Korzen based on Udemy course
+
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
---
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -0,0 +1,17 @@
+// Wawrzyniec Korzen based on Udemy course
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AuraAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
