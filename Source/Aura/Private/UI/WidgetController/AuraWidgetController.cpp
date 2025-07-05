d1ee9cef05d50693637289401c4c684fff007ed0
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2025-07-05 15:09:29 +0200

    Widget base class and WidgetController class added

---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Wawrzyniec Korzen based on Udemy course
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
---
+++ Source/Aura/Private/UI/Widgets/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Wawrzyniec Korzen based on Udemy course
+
+
+#include "UI/Widgets/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
