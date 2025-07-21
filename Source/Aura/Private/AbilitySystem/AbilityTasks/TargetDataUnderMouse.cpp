72cb767b9c0fbcb9aec4dcc7c4e6383477a339cf
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2025-07-21 16:51:13 +0200

    TargetDataUnderMouse Ability Task created and set to boradcast mouse click location

---
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -0,0 +1,21 @@
+// Wawrzyniec Korzen based on Udemy course
+
+
+#include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
+
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
+	
+	return MyObj;
+}
+
+void UTargetDataUnderMouse::Activate()
+{
+	//super not needed here
+	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	FHitResult CursorHit;
+	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	ValidData.Broadcast(CursorHit.Location);
+}
