79539c316411dccad45b26794046bcc6baee879c
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2025-08-15 12:35:15 +0200

    Added Ghoul enemy BP, ABP and HitRact, Left Hand and Right Hand Montages + some debugging messages for highlighting enemies

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -112,6 +112,7 @@ void AAuraPlayerController::CursorTrace()
 		if (ThisActor)
 		{
 			ThisActor->HighlightActor();
+			GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, TEXT("Highlight Actor Set!!"));	
 		}
 	}
 	
