b7f37a6f98c4ca4876845208343f1cadb00aa049
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2025-10-16 15:12:32 +0200

    Added showing XP in HUD: Set WidgetController for XPBar and handled updating XP bar percentage to be shown. Some changes in mana crystals and potions to make them work better

---
+++ Source/Aura/Private/Interaction/PlayerInterface.cpp
@@ -0,0 +1,6 @@
+// Wawrzyniec Korzen based on Udemy course
+
+
+#include "Interaction/PlayerInterface.h"
+
+// Add default functionality here for any IPlayerInterface functions that are not pure virtual.
