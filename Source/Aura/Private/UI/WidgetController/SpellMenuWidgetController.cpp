bb50bdb7b4aecc1508d5945c76a98733a3a91638
Wawrzyniec <wawrzyniec.korzen@gmail.com>
2026-02-21 17:03:07 +0100

    Spell Menu: Added working of spending Spell Points to level up a Spell or Unlock Eligible Spell (Ability). Added necessa\ry functions in SpellMenu Widget Controller, ASC and in Spell Menu Blueprint

--- Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -17,7 +17,7 @@ void USpellMenuWidgetController::BroadcastInitialValues()
 
 void USpellMenuWidgetController::BindCallbacksToDependencies()
 {
-	GetAuraASC()->AbilityStatusChangedDelegate.AddLambda([this](const FGameplayTag& AbilityTag, const FGameplayTag& StatusTag)
+	GetAuraASC()->AbilityStatusChangedDelegate.AddLambda([this](const FGameplayTag& AbilityTag, const FGameplayTag& StatusTag, int32 NewLevel)
 		{
 		if (SelectedAbility.Ability.MatchesTagExact(AbilityTag))
 		{
@@ -79,6 +79,13 @@ void USpellMenuWidgetController::SpellGlobeSelected(const FGameplayTag& AbilityT
 	ShouldEnableButtons(AbilityStatus, SpellPoints, bEnableSpendPoints,bEnableEquip );
 	SpellGlobeSelectedDelegate.Broadcast(bEnableSpendPoints, bEnableEquip);
 }
+
+void USpellMenuWidgetController::SpendPointButtonPressed()
+{
+	if (GetAuraASC())
+		GetAuraASC()->ServerSpendSpellPoint(SelectedAbility.Ability);
+}
+
 
 void USpellMenuWidgetController::ShouldEnableButtons(const FGameplayTag& AbilityStatus, int32 SpellPoints,
 	bool& bShouldEnableSpellPointsButton, bool& bShouldEnableEquipButton)
